2fc6c9b5b5804850940ef189c90bc76494f429aa
Siddhesh Acharekar <siddheshsacharekar@gmail.com>
2019-07-11 21:06:43 -0400

    Setting up structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Tensorflow and Keras implementations of technologies and algorithms studied as part of CSCI 89A Deep Learning in NLP.',
+    author='Siddhesh Acharekar',
+    license='MIT',
+)
